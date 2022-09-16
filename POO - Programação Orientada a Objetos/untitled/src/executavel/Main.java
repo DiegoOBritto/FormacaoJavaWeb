315281313224193d720ae00e20b1881ef3f80b2d
DiegoOBritto <diegoob71@gmail.com>
2022-09-16 15:06:49 -0300

    Criando e percorrendo a lista de alunos

--- POO - Programação Orientada a Objetos/untitled/src/executavel/Main.java
+++ POO - Programação Orientada a Objetos/untitled/src/executavel/Main.java
@@ -4,13 +4,19 @@ import classesJava.Aluno;
 import classesJava.Disciplina;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String nome = JOptionPane.showInputDialog("Digite o nome do aluno ? ");
-        String idade = JOptionPane.showInputDialog("Digite a idade do aluno ?");
+        List<Aluno> alunos = new ArrayList<Aluno>();
+
+        for (int qtd = 1; qtd <= 2; qtd++) {
+
+            String nome = JOptionPane.showInputDialog("Digite o nome do aluno " + qtd + " ? ");
+       /* String idade = JOptionPane.showInputDialog("Digite a idade do aluno ?");
         String nascimento = JOptionPane.showInputDialog("Digite a data de nascimento do aluno ?");
         String rg = JOptionPane.showInputDialog("Digite o RG do aluno ?");
         String cpf = JOptionPane.showInputDialog("Digite o CPF do aluno ?");
@@ -18,16 +24,16 @@ public class Main {
         String pai = JOptionPane.showInputDialog("Digite o nome do pai do aluno: ");
         String matricula = JOptionPane.showInputDialog("Digite a data da matricula do aluno: ");
         String escola = JOptionPane.showInputDialog("Digite o nome da escola: ");
-        String serie = JOptionPane.showInputDialog("Digite a série em que o aluno está matriculado");
+        String serie = JOptionPane.showInputDialog("Digite a série em que o aluno está matriculado");*/
 
 
-        //aluno1, aluno2...são referencias para o objeto aluno
-        //new Aluno() é uma instancia (criação de um Objeto)
-        //Construtor padrão
-        Aluno aluno1 = new Aluno();
+            //aluno1, aluno2...são referencias para o objeto aluno
+            //new Aluno() é uma instancia (criação de um Objeto)
+            //Construtor padrão
+            Aluno aluno1 = new Aluno();
 
-        aluno1.setNome(nome);
-        aluno1.setIdade(Integer.valueOf(idade));
+            aluno1.setNome(nome);
+        /*aluno1.setIdade(Integer.valueOf(idade));
         aluno1.setDataNascimento(nascimento);
         aluno1.setRegistroGeral(rg);
         aluno1.setNumeroCpf(cpf);
@@ -35,37 +41,44 @@ public class Main {
         aluno1.setNomePai(pai);
         aluno1.setDataMatricula(matricula);
         aluno1.setNomeEscola(escola);
-        aluno1.setSerieMatriculado(serie);
+        aluno1.setSerieMatriculado(serie);*/
 
-        for (int pos = 1; pos <= 4; pos++) {
-            String nomeDisciplina = JOptionPane.showInputDialog("Informe a disciplina: " + pos);
-            String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina: " + pos);
+            for (int pos = 1; pos <= 4; pos++) {
+                String nomeDisciplina = JOptionPane.showInputDialog("Informe a disciplina: " + pos);
+                String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina: " + pos);
 
-            Disciplina disciplina = new Disciplina();
-            disciplina.setDisciplina(nomeDisciplina);
-            disciplina.setNota(Double.valueOf(notaDisciplina));
+                Disciplina disciplina = new Disciplina();
+                disciplina.setDisciplina(nomeDisciplina);
+                disciplina.setNota(Double.valueOf(notaDisciplina));
 
-            aluno1.getDisciplinas().add(disciplina);
-        }
-
-        int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ?");
-
-        if (escolha == 0) {
-
-            int conitnuarRemover = 0; //Opcão "sim" = 0
-            int posicao = 1;
-
-            while (conitnuarRemover == 0) {
-                String disciplinaRemover = JOptionPane.showInputDialog("Qual disciplina deseja remover: 1, 2, 3 ou 4 ? ");
-                aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
-                posicao ++;
-                conitnuarRemover = JOptionPane.showConfirmDialog(null, "Deseja remover outra disciplina ?");
+                aluno1.getDisciplinas().add(disciplina);
             }
+
+            int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ?");
+
+            if (escolha == 0) {
+
+                int conitnuarRemover = 0; //Opcão "sim" = 0
+                int posicao = 1;
+
+                while (conitnuarRemover == 0) {
+                    String disciplinaRemover = JOptionPane.showInputDialog("Qual disciplina deseja remover: 1, 2, 3 ou 4 ? ");
+                    aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+                    posicao++;
+                    conitnuarRemover = JOptionPane.showConfirmDialog(null, "Deseja remover outra disciplina ?");
+                }
+            }
+
+            alunos.add(aluno1);
+
         }
 
-        System.out.println(aluno1.toString()); //Descrição do obejto na memoria
-        System.out.println("Média do aluno: " + aluno1.getMediaNota());
-        System.out.println("Resultado: " + aluno1.getAlunoAprovado2());
+        for (Aluno aluno : alunos) {
 
+            System.out.println(aluno.toString()); //Descrição do obejto na memoria
+            System.out.println("Média do aluno: " + aluno.getMediaNota());
+            System.out.println("Resultado: " + aluno.getAlunoAprovado2());
+            System.out.println("===============================================");
+        }
     }
 }
