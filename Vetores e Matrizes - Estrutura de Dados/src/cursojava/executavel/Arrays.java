3fd64bb2db592e42f733e4d0f31cd951d77f3f8a
DiegoOBritto <diegoob71@gmail.com>
2022-12-19 18:18:47 -0300

    Entrada de dados e leitura de forma dinâmica

--- Vetores e Matrizes - Estrutura de Dados/src/cursojava/executavel/Arrays.java
+++ Vetores e Matrizes - Estrutura de Dados/src/cursojava/executavel/Arrays.java
@@ -1,22 +1,22 @@
 package cursojava.executavel;
 
+import javax.swing.*;
+
 public class Arrays {
 
     public static void main(String[] args) {
 
-        /*Arrays podem ter todo tipo de dados e objetos*/
+        String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter ?");
 
-        /*Arrays sempre devem ter a quantidade de posições definidas*/
-        double[] notas = new double[5];
+        double[] notas = new double[Integer.parseInt(posicoes)];
 
-        //Atribuindo valor na posição de cada array
-        notas[0] = 6.8;
-        notas[1] = 9.7;
-        notas[2] = 5.5;
-        notas[3] = 3.0;
+        for (int pos = 0; pos < notas.length; pos ++) {
+            String valor = JOptionPane.showInputDialog("Qual o valor da posição " + (pos + 1) + " ?");
+            notas[pos] = Double.valueOf(valor);
+        }
 
-       for (int pos = 0; pos < 5; pos ++) {
-           System.out.println("Nota " + (pos + 1)+ " é: " + notas[pos]);
-       }
+        for (int pos = 0; pos < notas.length; pos++) {
+            System.out.println("Nota " + (pos + 1) + " é: " + notas[pos]);
+        }
     }
 }
