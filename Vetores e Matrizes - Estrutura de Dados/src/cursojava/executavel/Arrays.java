5b0b3e298d0efc5640e723474ed1416af19bc37f
DiegoOBritto <diegoob71@gmail.com>
2022-12-19 18:01:38 -0300

    Recuperando valores no Array

--- Vetores e Matrizes - Estrutura de Dados/src/cursojava/executavel/Arrays.java
+++ Vetores e Matrizes - Estrutura de Dados/src/cursojava/executavel/Arrays.java
@@ -7,7 +7,7 @@ public class Arrays {
         /*Arrays podem ter todo tipo de dados e objetos*/
 
         /*Arrays sempre devem ter a quantidade de posições definidas*/
-        double[] notas = new double[4];
+        double[] notas = new double[5];
 
         //Atribuindo valor na posição de cada array
         notas[0] = 6.8;
@@ -15,6 +15,8 @@ public class Arrays {
         notas[2] = 5.5;
         notas[3] = 3.0;
 
-        System.out.println(notas);
+       for (int pos = 0; pos < 5; pos ++) {
+           System.out.println("Nota " + (pos + 1)+ " é: " + notas[pos]);
+       }
     }
 }
